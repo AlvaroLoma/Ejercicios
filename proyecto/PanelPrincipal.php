2e08b053fc95943b71162d573fee75784e7bcdb3
mini_ <mini_yitan@hotmail.com>
2022-02-22 13:42:10 +0100

    4º

---
+++ proyecto/PanelPrincipal.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="PanelPrincipalStyle.css">
+    <title></title>
+  </head>
+
+  <body>
+    <header>
+      <h1>Panel de administracion</h1>
+    </header>
+    <div class="main">
+      <nav>
+        <ul>
+          <li>Oferta educativa</li>
+          <li>Asignatura</li>
+          <li>Profesor</li>
+          <li>Horario</li>
+        </ul>
+      </nav>
+      <div class="contenido">
+      </div>
+    </div>
+
+  </body>
+</html>
